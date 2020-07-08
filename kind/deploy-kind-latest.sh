c228ac26a3edd83f5d3dc0d1713ee991cd14af75
githubfoam <lequestionis@gmail.com>
2020-07-08 12:40:05 +0300

    ув`еренный

--- kind/deploy-kind-latest.sh
+++ kind/deploy-kind-latest.sh
@@ -9,10 +9,10 @@ set -o xtrace
 echo "=============================deploy kind============================================================="
 docker version
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /usr/local/bin/ # Download kubectl
-kubectl version --client
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 && chmod 700 get_helm.sh && bash get_helm.sh
-helm version
+# curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /usr/local/bin/ # Download kubectl
+# kubectl version --client
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 && chmod 700 get_helm.sh && bash get_helm.sh
+# helm version
 
 export KIND_VERSION="0.8.1"
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v$KIND_VERSION/kind-$(uname)-amd64
